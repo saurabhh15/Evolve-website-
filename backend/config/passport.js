ac90afeee1c3e45360f05762c1cb660163a743ef
Saurabh Kumar <saurabhsin141@gmail.com>
2026-05-08 04:57:02 +0530

    again fix in passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -49,7 +49,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:5000/api/auth/github/callback',
+      callbackURL: 'https://evolve-website.onrender.com/api/auth/github/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
